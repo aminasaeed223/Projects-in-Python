e22131ddfca86cb1623604433857ba0e6451aaf2
aminasaeed223 <aminasaeed223@gmail.com>
2024-01-16 08:26:52 +0500

    commit commit

---
+++ word_replacemnt_program.py
@@ -0,0 +1,6 @@
+def replace_word(): # Defining a function to replace word
+    str = "Hi,My name is Amina."
+    word_to_replace = input("Enter the word to replace: ")
+    word = input("Enter the word replacement: ")
+    print(str.replace (word_to_replace, word ))# using.replace method
+replace_word()# calling the function
